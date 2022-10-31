020c4964e65e62eaab6bee52df7490c7dc516384
Nishant Gupta <guptanishant1307@gmail.com>
2022-10-31 20:36:34 +0530

    few fixes

--- src/components/CreateBill.js
+++ src/components/CreateBill.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { useState } from 'react'
 import { useDispatch } from 'react-redux'
+import { Navigate } from 'react-router-dom'
 import { createBill } from '../reducers/billSlice'
 
-
 const CreateBill = () => {
+  const [IsError, setIsError] = useState(0)
   const dispatch = useDispatch()
   const [NewBill, setNewBill] = useState({
     description: '',
@@ -12,8 +13,6 @@ const CreateBill = () => {
     amount: 0,
     category: '',
   })
-
-
 
   function onChangeHandler(e) {
     let { name, value } = e.target
@@ -27,8 +26,18 @@ const CreateBill = () => {
       (NewBill.amount !== 0) &
       (NewBill.category !== '')
     ) {
-        dispatch(createBill(NewBill))
+      dispatch(createBill(NewBill))
       clearForm()
+      setTimeout(() => {
+        setIsError(2)
+      }, 500)
+
+
+    }else{
+      setIsError(1)
+      setTimeout(() => {
+        setIsError(0)
+      }, 1000)
     }
   }
 
@@ -107,6 +116,11 @@ const CreateBill = () => {
           Clear
         </div>
       </div>
+      {IsError===2 ? (<Navigate to="/" replace={true}></Navigate>):null}
+
+      {IsError===1 ? (
+        <h5 style={{ color: 'red' }}>All Fields Are Required..</h5>
+      ) : null}
     </div>
   )
 }
